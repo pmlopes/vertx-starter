63a3623d843aad1ea8b4a79b36b5e13cad2010c6
Paulo Lopes <paulo@mlopes.net>
2016-11-04 10:41:09 +0100

    make it configurable

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # vertx-starter
+
+This is a simple online generator for vert.x projects
+
+The whole SPA is data driven, build tools can be added to the file [buildtools.json](buildtools.json).
+
+Components can be added to the file [components.json](components.json)
+
+The generated project comes from the handlebars templates under `templ` for each build tools there should be a folder. In this folder all files will be handled as handlebars templates.
--- js/app.js
+++ js/app.js
@@ -1,75 +1,65 @@
 var app = angular.module('starter', []);
 
-app.controller('MainCtrl', function ($scope) {
+app.controller('MainCtrl', function ($scope, $http) {
 
-  $scope.components = [
-    {
-      "name": "vertx-core",
-      "groupId": "io.vertx",
-      "artifactId": "vertx-core",
-      "version": "3.3.3",
-      "stack": true,
-      "description": "Core"
-    },
-    {
-      "name": "yoke",
-      "groupId": "com.jetdrone",
-      "artifactId": "yoke",
-      "version": "3.3.3",
-      "stack": false,
-      "description": "Web Framework"
-    }
-  ];
+  // this will hold all the components we allow in the simple project
+  $scope.components = [];
+  // this will hold all selected dependencies
+  $scope.dependencies = [];
 
-  $scope.selectMaven = function () {
-    $scope.selected = { maven: 'background: rgb(202, 60, 60)' };
-    $scope.fields = [
-      { key: 'name', value: 'Project Name' },
-      { key: 'description', value: 'Project Description' },
-      { key: 'groupId', value: 'Maven Group Id' },
-      { key: 'artifactId', value: 'Maven Artifact Id' },
-      { key: 'version', value: 'Project Version' },
-      { key: 'main', value: 'Main Verticle' }
-    ]
-    $scope.title = 'Maven pom.xml'
-    $scope.template = 'pom.xml'
-  }
+  // get the components from the server
+  $http.get('/components.json')
+    .then(function (res) {
+      // TODO: handle status != 200
+      $scope.components = res.data;
+    });
 
-  $scope.selectGradle = function () {
-    $scope.selected = { gradle: 'background: rgb(202, 60, 60)' };
-    $scope.fields = [
-      { key: 'name', value: 'Project Name' },
-      { key: 'description', value: 'Project Description' },
-      { key: 'version', value: 'Project Version' },
-      { key: 'main', value: 'Main Verticle' }
-    ]
-    $scope.title = 'Gradle build.gradle'
-    $scope.template = 'build.gradle'
-  }
+  // get the buildtools from the server
+  $http.get('/buildtools.json')
+    .then(function (res) {
+      // TODO: handle status != 200
+      $scope.buildtools = res.data;
+      // initially select the first element
+      $scope.selectBuildTool(0);
+    });
 
-  $scope.selectNPM = function () {
-    $scope.selected = { npm: 'background: rgb(202, 60, 60)' };
-    $scope.fields = [
-      { key: 'name', value: 'Project Name' },
-      { key: 'description', value: 'Project Description' },
-      { key: 'version', value: 'Project Version' },
-    ]
-    $scope.title = 'NPM package.json'
-    $scope.template = 'package.json'
-  }
+  $scope.selectBuildTool = function (idx) {
+    $scope.selected = {};
+    $scope.tool = $scope.buildtools[idx].name;
+    $scope.fields = $scope.buildtools[idx].fields;
+    $scope.title = $scope.buildtools[idx].metadata;
+    $scope.templates = $scope.buildtools[idx].templates;
 
-  $scope.dependencies = []
-
+    $scope.selected[$scope.buildtools[idx].name] = 'background: rgb(202, 60, 60)';
+  };
 
   $scope.generate = function () {
-    var template = Handlebars.templates[$scope.template];
+    ga('send', {
+      hitType: 'event',
+      eventCategory: $scope.tool,
+      eventAction: 'new'
+    });
+
+    // get all data from the form
     for (var i = 0; i < $scope.fields.length; i++) {
       $scope[$scope.fields[i].key] = document.getElementById($scope.fields[i].key).value;
     }
 
+    // create a new zip file
     var zip = new JSZip();
 
-    zip.file($scope.template, template($scope));
+    // for each template run
+    var file, fn, slash;
+
+    for (var i = 0; i < $scope.templates.length; i++) {
+      file = $scope.templates[i];
+      fn = Handlebars.templates[file];
+      slash = file.indexOf('/');
+      if (slash > 0) {
+        file = file.substr(slash + 1);
+      }
+      zip.file(file, fn($scope));
+    }
 
     if (JSZip.support.blob) {
       zip.generateAsync({ type: 'blob' }).then(function (blob) {
