a64d94847e845933479a376d9e32d7fef37c934c
Paulo Lopes <paulo@mlopes.net>
2017-09-25 11:49:06 +0200

    move to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,71 @@
 // Include gulp
 var gulp = require('gulp');
 
-// Include Our Plugins
+// Include Plugins
 var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var riot = require('gulp-riot');
 var download = require('gulp-downloader');
-var handlebars = require('gulp-handlebars');
 var wrap = require('gulp-wrap');
 var ghPages = require('gulp-gh-pages');
+// Custom Plugin
+var handlebars = function(opts) {
+  'use strict';
+
+  opts = opts || {};
+  var compilerOptions = opts.compilerOptions || {};
+  var handlebars = require('handlebars');
+  var through2 = require('through2');
+  var gutil = require('gulp-util');
+
+  return through2.obj(function(file, enc, callback) {
+    if (file.isNull()) {
+      return callback(null, file);
+    }
+
+    if (file.isStream()) {
+      this.emit('error', new gutil.PluginError('gulp-handlebars', 'Streaming not supported'));
+      return callback();
+    }
+
+    var contents = file.contents.toString();
+    var compiled = null;
+    try {
+      var ast = handlebars.parse(contents);
+      // Preprocess AST before compiling
+      if (opts.processAST) {
+        // processAST may return new AST or change it in place
+        ast = opts.processAST(ast) || ast;
+      }
+      compiled = handlebars.precompile(ast, compilerOptions).toString();
+    }
+    catch (err) {
+      this.emit('error', new gutil.PluginError('gulp-handlebars', err, {
+        fileName: file.path
+      }));
+      return callback();
+    }
+
+    file.contents = new Buffer(compiled);
+    file.templatePath = file.relative;
+    file.path = gutil.replaceExtension(file.path, '.js');
+
+    // Options that take effect when used with gulp-define-module
+    file.defineModuleOptions = {
+      require: {
+        Handlebars: 'handlebars'
+      },
+      context: {
+        handlebars: 'Handlebars.template(<%= contents %>)'
+      },
+      wrapper: '<%= handlebars %>'
+    };
+
+    callback(null, file);
+  });
+};
 
 // Google Analytics Task
 gulp.task('ga', function () {
@@ -23,10 +78,10 @@ gulp.task('vendor', ['ga'], function () {
   gulp
     .src(['node_modules/riot/riot.js', 'node_modules/riot-route/dist/route.js', 'node_modules/handlebars/dist/handlebars.runtime.js', 'node_modules/jszip/dist/jszip.js', 'node_modules/jszip-utils/dist/jszip-utils.js', 'js/analytics.js'])
     .pipe(concat('vendor.js'))
-    .pipe(gulp.dest('js'))
+    .pipe(gulp.dest('dist/js'))
     .pipe(rename('vendor.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('js'));
+    .pipe(gulp.dest('dist/js'));
 });
 
 // Riot Task
@@ -36,41 +91,44 @@ gulp.task('riot', function () {
       compact: true
     }))
     .pipe(concat('tags.js'))
-    .pipe(gulp.dest('js'));
+    .pipe(gulp.dest('dist/js'))
+    .pipe(rename('tags.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
 });
 
 // Lint Task
 gulp.task('lint', ['riot'], function () {
-  return gulp.src(['js/app.js', 'js/tags.js'])
+  return gulp.src(['js/app.js', 'dist/js/tags.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
 // Minify JS
 gulp.task('minify', ['riot'], function () {
-  return gulp.src(['js/app.js', 'js/tags.js'])
+  return gulp.src('js/app.js')
     .pipe(rename('app.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('js'));
+    .pipe(gulp.dest('dist/js'));
 });
 
 // Minify handlebars
 gulp.task('handlebars', function () {
   return gulp.src(['templ/**/*.*', 'templ/**/.*'])
     .pipe(handlebars())
-    .pipe(wrap('  Handlebars.templates[\'<%= file.relative %>\'] = Handlebars.template(<%= contents %>);\n'))
+    .pipe(wrap('  Handlebars.templates[\'<%= file.templatePath %>\'] = Handlebars.template(<%= contents %>);\n'))
     .pipe(concat('templates.js'))
-    .pipe(wrap('(function () {\n  <%= contents %>\n})();'))
-    .pipe(gulp.dest('js'))
+    .pipe(wrap('(function () {\n  Handlebars.templates = Handlebars.templates || {};\n  <%= contents %>\n})();'))
+    .pipe(gulp.dest('dist/js'))
     .pipe(rename('templates.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('js'));
+    .pipe(gulp.dest('dist/js'));
 
 });
 
 // Watch Files For Changes
 gulp.task('watch', function () {
-  gulp.watch('js/*.js', ['lint', 'minify']);
+  gulp.watch(['js/*.js', 'tags/*.tag'], ['lint', 'minify']);
 });
 
 // Default Task
@@ -78,7 +136,7 @@ gulp.task('default', ['vendor', 'handlebars', 'lint', 'minify', 'watch']);
 
 // Deploy to gh-pages
 gulp.task('deploy', ['vendor', 'handlebars', 'minify'], function () {
-  return gulp.src(['blobs', 'css', 'img', 'js/*.min.js', '*.json', 'index.html', 'CNAME'])
+  return gulp.src('dist/**/*')
     .pipe(ghPages({
       push: false
     }));
--- js/app.js
+++ js/app.js
@@ -1,196 +1,198 @@
-function loadJSON(file, callback) {
-  if (localStorage) {
-    var json = null; //localStorage.getItem(file);
+(function (self) {
+  self.loadJSON = function (file, callback) {
+    if (localStorage) {
+      var json = null; //localStorage.getItem(file);
 
-    if (json) {
-      json = JSON.parse(json);
-      if (json.version !== window.starterVersion) {
-        localStorage.removeItem(file);
-      } else if (json.ttl && json.ttl < Date.now()) {
-        localStorage.removeItem(file);
-      } else {
+      if (json) {
+        json = JSON.parse(json);
+        if (json.version !== window.starterVersion) {
+          localStorage.removeItem(file);
+        } else if (json.ttl && json.ttl < Date.now()) {
+          localStorage.removeItem(file);
+        } else {
+          callback(null, json.text);
+          return;
+        }
+      }
+    }
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', file, true);
+    xobj.onreadystatechange = function () {
+      if (xobj.readyState == 4) {
+        if (xobj.status != "200" && xobj.status != "304") {
+          return callback("Failed to load the metadata!");
+        }
+        var json = {
+          version: window.starterVersion,
+          // only cache for 30min
+          ttl: 1800000 + Date.now(),
+          text: xobj.responseText
+        };
+
+        if (localStorage) {
+          localStorage.setItem(file, JSON.stringify(json));
+        }
+
         callback(null, json.text);
-        return;
       }
-    }
-  }
-
-  var xobj = new XMLHttpRequest();
-  xobj.overrideMimeType("application/json");
-  xobj.open('GET', file, true);
-  xobj.onreadystatechange = function () {
-    if (xobj.readyState == 4) {
-      if (xobj.status != "200" && xobj.status != "304") {
-        return callback("Failed to load the metadata!");
-      }
-      var json = {
-        version: window.starterVersion,
-        // only cache for 30min
-        ttl: 1800000 + Date.now(),
-        text: xobj.responseText
-      };
-
-      if (localStorage) {
-        localStorage.setItem(file, JSON.stringify(json));
-      }
-
-      callback(null, json.text);
-    }
+    };
+    xobj.send(null);
   };
-  xobj.send(null);
-}
 
-function compileProject(project, callback) {
-  var i;
+  self.compileProject = function (project, callback) {
+    var i;
 
-  // merge executables from buildtool and preset
-  var executables = project.buildtool.executables || [];
-  if (project.preset) {
-    executables = executables.concat(project.preset.executables || []);
-  }
-
-  // track what project type is being generated
-  ga('send', {
-    hitType: 'event',
-    eventCategory: project.buildtool.id + ':project',
-    eventAction: project.buildtool.id + '/new',
-    eventLabel: 'project'
-  });
-
-  // track what dependencies are being selected
-  project.dependencies.forEach(function (el) {
-    // force boolean
-    el.checked = !!el.checked;
-
-    if (el.checked) {
-      ga('send', {
-        hitType: 'event',
-        eventCategory: project.buildtool.id + ':dependency',
-        eventAction: project.buildtool.id + '/' + el.groupId + ':' + el.artifactId + ':' + el.version,
-        eventLabel: 'dependency'
-      });
+    // merge executables from buildtool and preset
+    var executables = project.buildtool.executables || [];
+    if (project.preset) {
+      executables = executables.concat(project.preset.executables || []);
     }
-  });
 
-  // filter checked dependencies
-  project.selectedDependencies = project.dependencies.filter(function (el) {
-    return el.checked;
-  });
+    // track what project type is being generated
+    ga('send', {
+      hitType: 'event',
+      eventCategory: project.buildtool.id + ':project',
+      eventAction: project.buildtool.id + '/new',
+      eventLabel: 'project'
+    });
 
-  // transform checked dependencies to a object
-  project.selectedDependenciesGa = {};
-  project.selectedDependencies.forEach(function (el) {
-    project.selectedDependenciesGa[el.groupId + ':' + el.artifactId] = el.version;
-  });
+    // track what dependencies are being selected
+    project.dependencies.forEach(function (el) {
+      // force boolean
+      el.checked = !!el.checked;
 
-  project.metadata = {};
+      if (el.checked) {
+        ga('send', {
+          hitType: 'event',
+          eventCategory: project.buildtool.id + ':dependency',
+          eventAction: project.buildtool.id + '/' + el.groupId + ':' + el.artifactId + ':' + el.version,
+          eventLabel: 'dependency'
+        });
+      }
+    });
 
-  // make a boolean value for the languageId
-  var language = project.language.id;
-  if (language.indexOf(' ') !== -1) {
-    language = language.substring(0, language.indexOf(' '));
-  }
-  // the id is excluding the extras
-  project.metadata[language] = true;
-  project.metadata.artifactSuffix = project.buildtool['non-core-suffix'] || '';
+    // filter checked dependencies
+    project.selectedDependencies = project.dependencies.filter(function (el) {
+      return el.checked;
+    });
 
-  // convert the fields to a form structure
-  project.buildtool.fields.forEach(function (el) {
-    project.metadata[el.key] = el.value ? el.value : el.prefill;
-  });
+    // transform checked dependencies to a object
+    project.selectedDependenciesGa = {};
+    project.selectedDependencies.forEach(function (el) {
+      project.selectedDependenciesGa[el.groupId + ':' + el.artifactId] = el.version;
+    });
 
-  if (project.metadata.groupId) {
-    project.metadata.packageName = project.metadata.groupId + '.' + (project.metadata.artifactId || project.metadata.name.replace(/[ -]/g, '_'));
-  }
+    project.metadata = {};
 
-  // complete the main
-  project.metadata.main = project.language.main.replace('{package}', project.metadata.packageName || '');
+    // make a boolean value for the languageId
+    var language = project.language.id;
+    if (language.indexOf(' ') !== -1) {
+      language = language.substring(0, language.indexOf(' '));
+    }
+    // the id is excluding the extras
+    project.metadata[language] = true;
+    project.metadata.artifactSuffix = project.buildtool['non-core-suffix'] || '';
 
-  // create a new zip file
-  var zip = new JSZip();
+    // convert the fields to a form structure
+    project.buildtool.fields.forEach(function (el) {
+      project.metadata[el.key] = el.value ? el.value : el.prefill;
+    });
 
-  var templates = [];
+    if (project.metadata.groupId) {
+      project.metadata.packageName = project.metadata.groupId + '.' + (project.metadata.artifactId || project.metadata.name.replace(/[ -]/g, '_'));
+    }
 
-  // marge all templates to be processed
-  templates = templates.concat(project.buildtool.templates);
+    // complete the main
+    project.metadata.main = project.language.main.replace('{package}', project.metadata.packageName || '');
 
-  if (project.language.templates) {
-    templates = templates.concat(project.language.templates);
-  }
-  if (project.preset) {
-    templates = templates.concat(project.preset.templates || []);
-  }
-  project.selectedDependencies.forEach(function (el) {
-    templates = templates.concat(el.templates || []);
-  });
+    // create a new zip file
+    var zip = new JSZip();
+
+    var templates = [];
+
+    // marge all templates to be processed
+    templates = templates.concat(project.buildtool.templates);
+
+    if (project.language.templates) {
+      templates = templates.concat(project.language.templates);
+    }
+    if (project.preset) {
+      templates = templates.concat(project.preset.templates || []);
+    }
+    project.selectedDependencies.forEach(function (el) {
+      templates = templates.concat(el.templates || []);
+    });
 
     // build tool specific templates
-  for (i = 0; i < templates.length; i++) {
-    compile(project, templates[i], (executables || []).indexOf(templates[i]) !== -1, zip);
-  }
+    for (i = 0; i < templates.length; i++) {
+      compile(project, templates[i], (executables || []).indexOf(templates[i]) !== -1, zip);
+    }
 
-  // blobs
-  var blob = project.buildtool.blob;
-  if (project.preset) {
-    blob = project.preset.blob || blob;
-  }
+    // blobs
+    var blob = project.buildtool.blob;
+    if (project.preset) {
+      blob = project.preset.blob || blob;
+    }
 
-  if (blob) {
-    // get the buildtools from the server
-    JSZipUtils.getBinaryContent(blob, function (err, data) {
-      if (err) {
-        ga('send', 'exception', {
-          'exDescription': err,
-          'exFatal': true
-        });
-        callback(err);
-        return;
-      }
-
-      zip.loadAsync(data)
-        .then(function (val) {
-          callback(null, val);
-        })
-        .catch(function (ex) {
+    if (blob) {
+      // get the buildtools from the server
+      JSZipUtils.getBinaryContent(blob, function (err, data) {
+        if (err) {
           ga('send', 'exception', {
-            'exDescription': ex.message,
+            'exDescription': err,
             'exFatal': true
           });
           callback(err);
-        });
-    });
-  } else {
-    callback(null, zip);
+          return;
+        }
+
+        zip.loadAsync(data)
+          .then(function (val) {
+            callback(null, val);
+          })
+          .catch(function (ex) {
+            ga('send', 'exception', {
+              'exDescription': ex.message,
+              'exFatal': true
+            });
+            callback(err);
+          });
+      });
+    } else {
+      callback(null, zip);
+    }
+  };
+
+  function compile(project, file, exec, zip) {
+    var hbfile, zfile, fn;
+
+    // extract filename
+    var dot = file.indexOf('.');
+    var lslash = file.lastIndexOf('/');
+
+    // extra metdata
+    if (project.metadata.packageName) {
+      project.metadata.dirName = project.metadata.packageName.replace(/\./g, '/');
+    }
+    project.metadata.fileName = file.substring(lslash + 1, dot);
+
+    // process file name (replace package placeholder with real package for zip, ignore placeholder for handlebars)
+    zfile = file.replace('{package}', project.metadata.dirName || '');
+    // first path element is always ignored
+    zfile = zfile.substr(zfile.indexOf('/') + 1);
+    hbfile = file.replace('{package}/', '');
+
+    // locate handlebars template
+    fn = Handlebars.templates[hbfile];
+    // add to zip
+    if (exec) {
+      zip.file(project.metadata.name.replace(/[ -]/g, '_') + '/' + zfile, fn(project), {
+        unixPermissions: '755'
+      });
+    } else {
+      zip.file(project.metadata.name.replace(/[ -]/g, '_') + '/' + zfile, fn(project));
+    }
   }
-}
-
-function compile(project, file, exec, zip) {
-  var hbfile, zfile, fn;
-
-  // extract filename
-  var dot = file.indexOf('.');
-  var lslash = file.lastIndexOf('/');
-
-  // extra metdata
-  if (project.metadata.packageName) {
-    project.metadata.dirName = project.metadata.packageName.replace(/\./g, '/');
-  }
-  project.metadata.fileName = file.substring(lslash + 1, dot);
-
-  // process file name (replace package placeholder with real package for zip, ignore placeholder for handlebars)
-  zfile = file.replace('{package}', project.metadata.dirName || '');
-  // first path element is always ignored
-  zfile = zfile.substr(zfile.indexOf('/') + 1);
-  hbfile = file.replace('{package}/', '');
-
-  // locate handlebars template
-  fn = Handlebars.templates[hbfile];
-  // add to zip
-  if (exec) {
-    zip.file(project.metadata.name.replace(/[ -]/g, '_') + '/' + zfile, fn(project), {
-      unixPermissions: '755'
-    });
-  } else {
-    zip.file(project.metadata.name.replace(/[ -]/g, '_') + '/' + zfile, fn(project));
-  }
-}
+})(window);
