ac5f0a6dcabea976b11278cd1450292415d5a161
Paulo Lopes <paulo@mlopes.net>
2018-10-04 20:25:36 +0200

    Update 2018-10-04T18:25:32.659Z

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-riot.tag2('main', '<div if="{tool}" class="container"><div class="help-tip hide-phone"><p> The build tool can be changed using the hamburger menu on the left. For more information about this tool please visit <a href="{tool.url}">{tool.id}</a> website.<br><br> This is a progressive webapp, meaning that you can add it to your desktop and it can even run offline. You can boostrap your project even on the go without internet! </p></div><h1>{tool.id}: {tool.file}</h1><form onsubmit="{generate}"><div each="{f, i in tool.fields}" class="row"><virtual if="{i % 2 === 0}"><div class="col-6"><input name="{tool.fields[i].key}" type="{tool.fields[i].checkbox ? \'checkbox\' : \'text\'}" placeholder="{tool.fields[i].label + (tool.fields[i].prefill ? \' e.g.: \' + tool.fields[i].prefill : \'\')}" required="{tool.fields[i].required}"><label if="{tool.fields[i].checkbox}"><br><i>{tool.fields[i].label + (tool.fields[i].prefill ? \' e.g.: \' + tool.fields[i].prefill : \'\')}</i></label></div><div class="col-6"><input if="{tool.fields[i+1]}" name="{tool.fields[i+1].key}" type="{tool.fields[i+1].checkbox ? \'checkbox\' : \'text\'}" placeholder="{tool.fields[i+1].label + (tool.fields[i+1].prefill ? \' e.g.: \' + tool.fields[i+1].prefill : \'\')}" required="{tool.fields[i+1].required}"><label if="{tool.fields[i+1] && tool.fields[i+1].checkbox}"><br><i>{tool.fields[i+1].label + (tool.fields[i+1].prefill ? \' e.g.: \' + tool.fields[i+1].prefill : \'\')}</i></label></div></virtual></div><div if="{tool.languages || presets}" class="row"><div class="col-6"><select id="language" disabled="{!tool.languages || tool.languages.length == 0}" onchange="{changeLanguage}"><option each="{tool.languages}" riot-value="{id}">{id}</option></select></div><div class="col-6"><select id="preset" disabled="{!presetGroups || presetGroups.length == 0}" onchange="{changePreset}"><option value="">Empty Project</option><optgroup each="{g, i in presetGroups}" label="{i}"><option each="{g}" riot-value="{id}">{id}</option></optgroup></select></div></div><div if="{preset.fields}" class="row"><h5>Extra variables for {preset.id}:</h5></div><div each="{f, i in preset.fields}" class="row"><virtual if="{i % 2 === 0}"><div class="col-6"><input name="{preset.fields[i].key}" type="text" placeholder="{preset.fields[i].label + (preset.fields[i].prefill ? \' e.g.: \' + preset.fields[i].prefill : \'\')}" required="{preset.fields[i].required}"></div><div class="col-6"><input if="{preset.fields[i+1]}" name="{preset.fields[i+1].key}" type="text" placeholder="{preset.fields[i+1].label + (preset.fields[i+1].prefill ? \' e.g.: \' + preset.fields[i+1].prefill : \'\')}" required="{preset.fields[i+1].required}"></div></virtual></div><div class="row"><div class="col-6"><h1>Dependencies</h1></div><div class="col-6"><div id="not-found-tip" class="help-tip hide-phone"><p>Use this box to search for dependencies to add to your application, if a dependency is not present, please <a href="https://github.com/pmlopes/vertx-starter/issues/new">open an issue</a> so we can add the missing metadata!</p></div><input type="text" class="pull-right" placeholder="Search dependency..." onkeyup="{search}"></div></div><div each="{c, i in dependencies}" class="row"><virtual if="{i % 2 == 0}"><div class="col-6"><div class="{dependency: dependencies[i].checked}"><input name="dependencies" type="checkbox" riot-value="{i}" checked="{dependencies[i].checked}" onclick="{toggleDependency}"><div><span if="{dependencies[i].stack}" class="pull-right"><img src="img/stack.svg" width="16px"></span><strong>{dependencies[i].artifactId}</strong><hr><p if="{dependencies[i].description}">{dependencies[i].description}</p></div><br></div></div><div class="col-6"><div if="{dependencies[i+1]}" class="{dependency: dependencies[i+1].checked}"><input name="dependencies" type="checkbox" riot-value="{i+1}" checked="{dependencies[i+1].checked}" onclick="{toggleDependency}"><div><span if="{dependencies[i+1].stack}" class="pull-right"><img src="img/stack.svg" width="16px"></span><strong>{dependencies[i+1].artifactId}</strong><hr><p if="{dependencies[i+1].description}">{dependencies[i+1].description}</p></div><br></div></div></virtual></div><div if="{notfound}" class="row center"><i>Sorry! Cannot find what you\'re looking for, please &nbsp;<a href="https://github.com/pmlopes/vertx-starter/issues/new">open an issue</a>&nbsp; so we know about the missing metadata!</i></div><div class="row" id="interaction"><div class="col-8"><button name="submit" type="submit">Generate</button> &nbsp; <a id="download-btn" onclick="{clean}" href="#" show="{downloading}" ref="download" download="{name}.zip" style="padding: 1.1rem 3.5rem; margin: 1rem 0; background: #782b90; color: #f5f5f5; border-radius: 2px; border: none; font-size: 1.3rem; transition: all .2s ease">Download</a></div><div class="col-4"><div if="{generating}" class="spinner"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div></div></div></div></form><div class="row"><div class="center"><span>powered with <span style="color:#f00000">&lt;3</span> by <a href="https://github.com/pmlopes/vertx-starter/tree/gh-pages">github.com</a></span></div></div></div>', '', '', function(opts) {
+riot.tag2('main', '<div if="{tool}" class="container"><div class="help-tip hide-phone"><p> The build tool can be changed using the hamburger menu on the left. For more information about this tool please visit <a href="{tool.url}">{tool.id}</a> website.<br><br> This is a progressive webapp, meaning that you can add it to your desktop and it can even run offline. You can boostrap your project even on the go without internet! </p></div><h1>{tool.id}: {tool.file}</h1><form onsubmit="{generate}"><div each="{f, i in tool.fields}" class="row"><virtual if="{i % 2 === 0}"><div class="col-6"><input name="{tool.fields[i].key}" type="{tool.fields[i].checkbox ? \'checkbox\' : \'text\'}" placeholder="{tool.fields[i].label + (tool.fields[i].prefill ? \' e.g.: \' + tool.fields[i].prefill : \'\')}" required="{tool.fields[i].required}"><label if="{tool.fields[i].checkbox}"><br><i>{tool.fields[i].label + (tool.fields[i].prefill ? \' e.g.: \' + tool.fields[i].prefill : \'\')}</i></label></div><div class="col-6"><input if="{tool.fields[i+1]}" name="{tool.fields[i+1].key}" type="{tool.fields[i+1].checkbox ? \'checkbox\' : \'text\'}" placeholder="{tool.fields[i+1].label + (tool.fields[i+1].prefill ? \' e.g.: \' + tool.fields[i+1].prefill : \'\')}" required="{tool.fields[i+1].required}"><label if="{tool.fields[i+1] && tool.fields[i+1].checkbox}"><br><i>{tool.fields[i+1].label + (tool.fields[i+1].prefill ? \' e.g.: \' + tool.fields[i+1].prefill : \'\')}</i></label></div></virtual></div><div if="{tool.languages || presets}" class="row"><div class="col-6"><select id="language" disabled="{!tool.languages || tool.languages.length == 0}" onchange="{changeLanguage}"><option each="{tool.languages}" riot-value="{id}">{id}</option></select></div><div class="col-6"><select id="preset" disabled="{!presetGroups || presetGroups.length == 0}" onchange="{changePreset}"><option value="">Empty Project</option><optgroup each="{g, i in presetGroups}" label="{i}"><option each="{g}" riot-value="{id}">{id}</option></optgroup></select></div></div><div if="{preset.fields}" class="row"><h5>Extra variables for {preset.id}:</h5></div><div each="{f, i in preset.fields}" class="row"><virtual if="{i % 2 === 0}"><div class="col-6"><input name="{preset.fields[i].key}" type="text" placeholder="{preset.fields[i].label + (preset.fields[i].prefill ? \' e.g.: \' + preset.fields[i].prefill : \'\')}" required="{preset.fields[i].required}"></div><div class="col-6"><input if="{preset.fields[i+1]}" name="{preset.fields[i+1].key}" type="text" placeholder="{preset.fields[i+1].label + (preset.fields[i+1].prefill ? \' e.g.: \' + preset.fields[i+1].prefill : \'\')}" required="{preset.fields[i+1].required}"></div></virtual></div><div class="row"><div class="col-6"><h1>Dependencies</h1></div><div class="col-6"><div id="not-found-tip" class="help-tip hide-phone"><p>Use this box to search for dependencies to add to your application, if a dependency is not present, please <a href="https://github.com/pmlopes/vertx-starter/issues/new">open an issue</a> so we can add the missing metadata!</p></div><input type="text" class="pull-right" placeholder="Search dependency..." onkeyup="{search}"></div></div><div each="{c, i in dependencies}" class="row"><virtual if="{i % 2 == 0}"><div class="col-6"><div class="{dependency: dependencies[i].checked}"><input name="dependencies" type="checkbox" riot-value="{i}" checked="{dependencies[i].checked}" onclick="{toggleDependency}"><div><span if="{dependencies[i].stack}" class="pull-right">&nbsp;<img src="img/stack.svg" width="16px"></span><span if="{dependencies[i].npm}" class="pull-right"><img src="img/npm.svg" width="16px"></span><strong>{dependencies[i].artifactId}</strong><hr><p if="{dependencies[i].deprecated}"><strong>DEPRECATED!!!</strong></p><p if="{dependencies[i].description}">{dependencies[i].description}</p></div><br></div></div><div class="col-6"><div if="{dependencies[i+1]}" class="{dependency: dependencies[i+1].checked}"><input name="dependencies" type="checkbox" riot-value="{i+1}" checked="{dependencies[i+1].checked}" onclick="{toggleDependency}"><div><span if="{dependencies[i+1].stack}" class="pull-right">&nbsp;<img src="img/stack.svg" width="16px"></span><span if="{dependencies[i+1].npm}" class="pull-right"><img src="img/npm.svg" width="16px"></span><strong>{dependencies[i+1].artifactId}</strong><hr><p if="{dependencies[i+1].deprecated}"><strong>DEPRECATED!!!</strong></p><p if="{dependencies[i+1].description}">{dependencies[i+1].description}</p></div><br></div></div></virtual></div><div if="{notfound}" class="row center"><i>Sorry! Cannot find what you\'re looking for, please &nbsp;<a href="https://github.com/pmlopes/vertx-starter/issues/new">open an issue</a>&nbsp; so we know about the missing metadata!</i></div><div class="row" id="interaction"><div class="col-8"><button name="submit" type="submit">Generate</button> &nbsp; <a id="download-btn" onclick="{clean}" href="#" show="{downloading}" ref="download" download="{name}.zip" style="padding: 1.1rem 3.5rem; margin: 1rem 0; background: #782b90; color: #f5f5f5; border-radius: 2px; border: none; font-size: 1.3rem; transition: all .2s ease">Download</a></div><div class="col-4"><div if="{generating}" class="spinner"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div></div></div></div></form><div class="row"><div class="center"><span>powered with <span style="color:#f00000">&lt;3</span> by <a href="https://github.com/pmlopes/vertx-starter/tree/gh-pages">github.com</a></span></div></div></div>', '', '', function(opts) {
     var self = this;
 
     var r = route.create();
@@ -141,7 +141,7 @@ riot.tag2('main', '<div if="{tool}" class="container"><div class="help-tip hide-
 
       var selection = [].concat(self.dependencies);
 
-      if (oldLang) {
+      if (oldLang && !oldLang.noLangSupport) {
         for (var index = 0; index < selection.length; index++) {
           var el = selection[index];
           if (el.groupId === 'io.vertx' && el.artifactId === ('vertx-lang-' + oldLang.id)) {
@@ -150,16 +150,18 @@ riot.tag2('main', '<div if="{tool}" class="container"><div class="help-tip hide-
         }
       }
 
-      opts.components.forEach(function (el, index) {
-        if (el.groupId === 'io.vertx' && el.artifactId === ('vertx-lang-' + newLang.id)) {
-          if (selection.filter(function (el2) { return el2.id === index; }).length === 0) {
-            var c = clone(el);
-            c.checked = true;
-            c.id = index;
-            selection.push(c);
+      if (newLang && !newLang.noLangSupport) {
+        opts.components.forEach(function (el, index) {
+          if (el.groupId === 'io.vertx' && el.artifactId === ('vertx-lang-' + newLang.id)) {
+            if (selection.filter(function (el2) { return el2.id === index; }).length === 0) {
+              var c = clone(el);
+              c.checked = true;
+              c.id = index;
+              selection.push(c);
+            }
           }
-        }
-      });
+        });
+      }
 
       var filteredPresets = filterPresets(self.tool.id, e.target.value);
       var filteredPresetsGroups = {};
@@ -402,6 +404,7 @@ riot.tag2('navigation', '<li each="{opts.buildtools}" class="nav-item"><a href="
 
 (function (self) {
   'use strict';
+
   function Clone() {
   }
 
@@ -425,6 +428,11 @@ riot.tag2('navigation', '<li each="{opts.buildtools}" class="nav-item"><a href="
     xobj.send(null);
   };
 
+})(window);
+
+(function (self) {
+  'use strict';
+
   self.compileProject = function (project, callback) {
     var i;
 
@@ -444,11 +452,25 @@ riot.tag2('navigation', '<li each="{opts.buildtools}" class="nav-item"><a href="
 
     // alias for selected dependencies
     project.dependenciesGAV = {};
+    // filtered dependencies by scope "dev", "prod", "mvn"
+    project.npmDevDependencies = [];
+    project.npmProdDependencies = [];
+    project.npmMavenDependencies = [];
 
     project.dependencies.forEach(function (el) {
       project.dependenciesGAV[el.groupId + ':' + el.artifactId] = el.version;
       if (el.classifier) {
         project.dependenciesGAV[el.groupId + ':' + el.artifactId + ':' + el.classifier] = el.version;
+      }
+
+      if (el.npm) {
+        if (el.scope === 'test') {
+          project.npmDevDependencies.push(el);
+        } else {
+          project.npmProdDependencies.push(el);
+        }
+      } else {
+        project.npmMavenDependencies.push(el);
       }
 
       // track what dependencies are being selected
@@ -545,7 +567,10 @@ riot.tag2('navigation', '<li each="{opts.buildtools}" class="nav-item"><a href="
     }
     // merge dependency specific templates
     project.dependencies.forEach(function (el) {
+      // language agnostic
       templates = templates.concat(el.templates || []);
+      // language + dependency specific
+      templates = templates.concat(el[project.language.id + 'Templates'] || []);
     });
 
     // build tool specific templates
@@ -580,8 +605,7 @@ riot.tag2('navigation', '<li each="{opts.buildtools}" class="nav-item"><a href="
           }
         }).then(function (val) {
           callback(null, val);
-        })
-          .catch(function (ex) {
+        }).catch(function (ex) {
             ga('send', 'exception', {
               'exDescription': ex.message,
               'exFatal': true
