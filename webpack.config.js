cf48bf658eb2f3cc1a9b7e773253a42ef09380ac
Paulo Lopes <paulo@mlopes.net>
2018-10-09 09:55:29 +0200

    disabled hot relead as we're using gulp to build

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
-var path = require('path')
+var path = require('path');
 
 module.exports = {
-    entry: './src/web_entrypoint.js',
-    output: {
-        path: path.resolve(__dirname, 'dist', 'js'),
-        filename: 'bundle.js'
-    },
-    mode: "production",
-    devtool: 'source-map',
-    target: 'web',
-    module: {
-        rules: [
-            {
-                test: /\.tag$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: 'riot-tag-loader',
-                    options: {
-                        hot: true,
-                        type: 'es6'
-                    }
-                }]
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-        ]
-    }
-}
+  entry: './src/web_entrypoint.js',
+  output: {
+    path: path.resolve(__dirname, 'dist', 'js'),
+    filename: 'bundle.js'
+  },
+  mode: "production",
+  devtool: 'source-map',
+  target: 'web',
+  module: {
+    rules: [
+      {
+        test: /\.tag$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'riot-tag-loader',
+          options: {
+            hot: false,
+            type: 'es6'
+          }
+        }]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
