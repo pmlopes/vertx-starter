5981ab09afc0067c0b2f9e902bce3a79538a0be9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-07 15:52:06 +0000

    Deploy pmlopes/vertx-starter to github.com/pmlopes/vertx-starter.git:gh-pages

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 // Names of the two caches used in this version of the service worker.
 // The id should expand from git on every deploy.
-const PRECACHE = 'v0.1.78';
+const PRECACHE = 'v0.1.80';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
